7d12fea83b74a9d4893ce34c0cdb6727b9f5ee46
zhongixn <zhongxing@wudi360.com>
2020-06-03 16:11:38 +0800

    fix

--- app/Command/ImpressionCommand.php
+++ app/Command/ImpressionCommand.php
@@ -100,8 +100,7 @@ class ImpressionCommand extends HyperfCommand
                     // var_dump(\Hyperf\Utils\Coroutine::inCoroutine());
                     $n++;
                     // $this->logger->info($n.PHP_EOL.'list:  '.$msg[0]. PHP_EOL . 'url:  '.$msg[1]);
-                    $this->logger->info($n.PHP_EOL.'list:  '.$msg[0]. PHP_EOL);
-                    // $this->logger->info($n);
+                    $this->logger->info($n);
                 }
             } catch (\Exception $e) {
                 Db::table('log_impressions')->insert([
