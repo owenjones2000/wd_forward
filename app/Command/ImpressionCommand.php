6aacd9554a4420f988165535cff3237cbe74254b
zhongixn <zhongxing@wudi360.com>
2020-06-05 09:29:57 +0800

    debug

--- app/Command/ImpressionCommand.php
+++ app/Command/ImpressionCommand.php
@@ -102,7 +102,7 @@ class ImpressionCommand extends HyperfCommand
                     // var_dump($stringBody);
                     // $headers = $response->getHeaders();
                     // var_dump(\Hyperf\Utils\Coroutine::inCoroutine());
-                    $n++;
+                    // $n++;
                     // $this->logger->info($n.PHP_EOL.'list:  '.$msg[0]. PHP_EOL . 'url:  '.$msg[1]);
                     // $this->logger->info($n);
                 }
