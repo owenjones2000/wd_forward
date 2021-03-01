9a25a23629c88946e063295c94a69b8a35e39b48
zhongixn <zhongxing@wudi360.com>
2021-03-01 17:39:25 +0800

    opt

--- app/Command/NetworkCommand.php
+++ app/Command/NetworkCommand.php
@@ -82,7 +82,6 @@ class NetworkCommand extends HyperfCommand
                     }
                 }
                 
-                
             } catch (\Exception $e) {
                 
                 $this->logger->error($e);
